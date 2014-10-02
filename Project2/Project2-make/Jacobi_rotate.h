df855006afd578ef9ac24ac680a16c36cded9f70
Emil Blix Wisborg <emilblix@gmail.com>
2014-10-03 00:54:03 +0200

    rename folder

---
+++ Project2/Project2-make/Jacobi_rotate.h
@@ -0,0 +1,11 @@
+#ifndef JACOBI_ROTATE_H
+#define JACOBI_ROTATE_H
+#include <armadillo>
+
+using namespace arma; // MÅ ha for å kunne bruke mat, vec i funksjoner, fjerner nødvendighet for denne i cpp-fila
+
+void Jacobi_rotate(mat &A, int k, int l, int n);
+//første ord (int, double, vec, mat) MÅ være samme type som ønsket output (void gir ingen output/return)
+
+
+#endif // JACOBI_ROTATE_H
